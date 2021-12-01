ef3d02658eeec02a0584a43289ffeffe6b585b62
ananchenko346 <52131256+ananchenko346@users.noreply.github.com>
2021-12-01 22:52:09 +0200

    Delete task_1.js

--- task_1.js
+++
@@ -1,9 +0,0 @@
-function getModifiedArray(array) {
-
-    let newArray = [...array];
-    newArray.splice(0, 1, 'Start');
-    newArray.splice(newArray.length - 1, 1, 'End');
-    return newArray;
-};
-
-getModifiedArray([12, 6, 22, 0, -8]);
