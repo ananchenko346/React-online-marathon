0a3c31b8c91eff13e588b385e04fbcc1ed77368d
Vlad <ananchenko346@gmail.com>
2021-12-14 22:24:39 +0200

    Sprints

--- Sprint-5/WebWorkers/script.js
+++ Sprint-5/WebWorkers/script.js
@@ -3,17 +3,17 @@ function calculate() {
 
   btn.addEventListener('click', () => {
 
-  const input = document.getElementById('inputNumber').value,
-        result = document.getElementById('result')
+    const input = document.getElementById('inputNumber').value,
+      result = document.getElementById('result')
 
     if (window.Worker) {
       const worker = new Worker('./worker.js')
 
       result.innerText = 'Calculating...'
-     
+
       worker.onmessage = message => {
         result.innerText = `Result: ${message.data}`
-      }     
+      }
       worker.postMessage(input)
     }
   })
