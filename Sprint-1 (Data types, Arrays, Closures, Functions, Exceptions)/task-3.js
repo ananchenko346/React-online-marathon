7fe199adca546e6f6174a0faa816b522a4c142ec
Vlad <ananchenko346@gmail.com>
2022-01-21 21:51:31 +0200

    Sprints

--- Sprint-1 (Data types, Arrays, Closures, Functions, Exceptions)/task-3.js
+++ Sprint-1 (Data types, Arrays, Closures, Functions, Exceptions)/task-3.js
@@ -1,6 +1,6 @@
 function longestLogin(loginList) {
-    let login = loginList.reduce((a, b) => a.length > b.length ? a : b);
-    return login;
+  let login = loginList.reduce((a, b) => a.length > b.length ? a : b);
+  return login;
 }
 
 longestLogin(["serg22", "tester_2", "Prokopenko", "guest"]);
--- Sprint-1 (Data types, Arrays, Closures, Functions, Exceptions)/task-6.js
+++ Sprint-1 (Data types, Arrays, Closures, Functions, Exceptions)/task-6.js
@@ -1,30 +1,30 @@
 const accountPatients = (counter) => {
-    let beds = counter;
-    let patients = 0;
+  let beds = counter;
+  let patients = 0;
 
-    function admit() {
-        beds = counter - 1;
-        patients += 1;
-        counter -= 1;
-        if (beds >= 0) {
-            console.log(`A patient was admitted, ${beds} beds are available`);
-        } else if (beds < 0) {
-            console.log('Can not admit a patient, no beds available')
-            counter = 0;
-        }
+  function admit() {
+    beds = counter - 1;
+    patients += 1;
+    counter -= 1;
+    if (beds >= 0) {
+      console.log(`A patient was admitted, ${beds} beds are available`);
+    } else if (beds < 0) {
+      console.log('Can not admit a patient, no beds available')
+      counter = 0;
     }
+  }
 
-    function discharge() {
-        beds = counter + 1;
-        patients -= 1;
-        counter += 1;
-        if (patients > 0) {
-            console.log(`A patient was discharged, ${beds} beds are available`);
-        } else if (patients <= 0) {
-            console.log('There are no patients to discharge')
-        }
+  function discharge() {
+    beds = counter + 1;
+    patients -= 1;
+    counter += 1;
+    if (patients > 0) {
+      console.log(`A patient was discharged, ${beds} beds are available`);
+    } else if (patients <= 0) {
+      console.log('There are no patients to discharge')
     }
-    return [admit, discharge];
+  }
+  return [admit, discharge];
 }
 
 const [admit, discharge] = accountPatients(3);
